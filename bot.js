72ecc2a60cc4916d78138009d879479b3e23c800
magicbankdis <43651158+magicbankdis@users.noreply.github.com>
2018-10-14 22:13:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,8 +30,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-
+client.login(process.env.BOT_TOKEN2);
 client.login(process.env.SPAM);
 heros.login(process.env.HER);
 other.login(process.env.OTH);
